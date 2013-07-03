46b6449e5362502ba9502fc723da9e0178874b50
Liz Abinante <eabinante@gmail.com>
2013-07-03 12:11:36 -0500

    users can login and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render layout: false
+  end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      create_session
+      redirect_to user_path(@user.id)
+    else # Error handling needed
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
