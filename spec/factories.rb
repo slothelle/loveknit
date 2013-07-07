a3a80afe6d9e213d1eae08c2909d2d17d096d420
Liz Abinante <eabinante@gmail.com>
2013-07-06 19:16:15 -0500

    created factories and spec_helper

---
+++ spec/factories.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  factory :user do
+    first_name    "Suzy"
+    last_name     "Queue"
+    ravelry_name  "suzyqqqq"
+    email         "suzy@queue.com"
+    password      "password123456"
+  end
+
+  factory :garment do
+    category "hat"
+  end
+
+  factory :needle do
+    size "US 6 (4.0 mm)"
+  end
+
+  factory :yarn_weight do
+    weight          "Aran"
+    yardage_factor  "1.2"
+  end
+end
