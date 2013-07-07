20c6adc3a6a5797773b5889322c7c9d7f38c1c44
Liz Abinante <eabinante@gmail.com>
2013-07-07 17:53:50 -0500

    update specs to include new associations; write specs for hat_patterns table

--- spec/models/needle_spec.rb
+++ spec/models/needle_spec.rb
@@ -3,5 +3,4 @@ require 'spec_helper'
 describe Needle do
   it { should be_instance_of(Needle) }
   it { should allow_mass_assignment_of(:size) }
-  it { should have_many(:patterns) }
 end
--- spec/models/pattern_spec.rb
+++ spec/models/pattern_spec.rb
@@ -3,23 +3,14 @@ require 'spec_helper'
 describe Pattern do
   it { should be_instance_of(Pattern) }
   it { should belong_to(:user) }
-  it { should belong_to(:needle) }
   it { should belong_to(:garment) }
-  it { should belong_to(:yarn_weight) }
-  it { should allow_mass_assignment_of(:name) }
-  it { should allow_mass_assignment_of(:gauge_row_inch) }
-  it { should allow_mass_assignment_of(:gauge_per_inch) }
-  it { should allow_mass_assignment_of(:hat_circumference) }
+  it { should belong_to(:hat_pattern) }
   it { should allow_mass_assignment_of(:user_id) }
-  it { should allow_mass_assignment_of(:needle_id) }
   it { should allow_mass_assignment_of(:garment_id) }
-  it { should allow_mass_assignment_of(:yarn_weight_id) }
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:gauge_row_inch) }
-  it { should validate_presence_of(:gauge_per_inch) }
-  it { should validate_presence_of(:hat_circumference) }
+  it { should allow_mass_assignment_of(:hat_pattern_id) }
+  it { should allow_mass_assignment_of(:user) }
+  it { should allow_mass_assignment_of(:garment) }
+  it { should allow_mass_assignment_of(:hat_pattern) }
   it { should validate_presence_of(:user_id) }
-  it { should validate_presence_of(:needle_id) }
   it { should validate_presence_of(:garment_id) }
-  it { should validate_presence_of(:yarn_weight_id) }
 end
