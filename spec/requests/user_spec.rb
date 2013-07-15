9a56a74f389d53e1d4b5b9201c63926bd3d356d7
Liz Abinante <eabinante@gmail.com>
2013-07-15 12:05:13 -0500

    all user specs passing

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -26,64 +26,39 @@ describe "User" do
       expect(page).to have_content("Wrong password")
     end
 
-  #   it "fails with invalid email" do
-  #     FactoryGirl.create(:user)
-  #     visit new_session_path
-  #     fill_in("Email", :with => "joe")
-  #     fill_in("Password", :with => "password1234")
-  #     click_button("Log In")
-  #     expect(page).to have_content("An account with the email address joe not found")
-  #   end
-  # end
+    it "fails with invalid email" do
+      FactoryGirl.create(:user)
+      visit login_path
+      fill_in("session[email]", :with => "suzy")
+      fill_in("session[password]", :with => "password1234")
+      click_button("Login")
+      expect(page).to have_content("An account with the email address suzy not found")
+    end
+  end
 
-  # context "account creation" do
-  #   it "succeeds with valid inputs" do
-  #     pending "No clue how to test this with ActionMailer"
-  #   end
+  context "account creation" do
+    it "succeeds with valid inputs" do
+      visit root_path
+      click_link("Signup")
+      fill_in("user[first_name]", :with => "Taco")
+      fill_in("user[last_name]", :with => "Meat")
+      fill_in("user[ravelry_name]", :with => "meaty")
+      fill_in("user[email]", :with => "meaty@tacos.com")
+      fill_in("user[password]", :with => "password1234")
+      click_button("Signup")
+      expect(page).to have_content("Hello, Taco")
+    end
 
-  #   it "fails with invalid short name" do
-  #     visit new_user_path
-  #     fill_in("Name", :with => "L")
-  #     fill_in("Email", :with => "joe@joe.com")
-  #     fill_in("Password", :with => "password1234")
-  #     click_button("Sign Up")
-  #     expect(page).to have_content("must be at least 2 characters.")
-  #   end
-
-  #   it "fails with invalid long name" do
-  #     visit new_user_path
-  #     fill_in("Name", :with => "Lllllllllllllllllllllllllllllllllllllllllll")
-  #     fill_in("Email", :with => "joe@joe.com")
-  #     fill_in("Password", :with => "password1234")
-  #     click_button("Sign Up")
-  #     expect(page).to have_content("must be less than 20 characters.")
-  #   end
-
-  #   it "fails with invalid email" do
-  #     visit new_user_path
-  #     fill_in("Name", :with => "Joe")
-  #     fill_in("Email", :with => "joe")
-  #     fill_in("Password", :with => "password1234")
-  #     click_button("Sign Up")
-  #     expect(page).to have_content("Email is invalid")
-  #   end
-
-  #   it "fails with invalid short password" do
-  #     visit new_user_path
-  #     fill_in("Name", :with => "Joe")
-  #     fill_in("Email", :with => "joe@joe.com")
-  #     fill_in("Password", :with => "pass")
-  #     click_button("Sign Up")
-  #     expect(page).to have_content("must be at least 6 characters.")
-  #   end
-
-  #   it "fails with invalid long password" do
-  #     visit new_user_path
-  #     fill_in("Name", :with => "Joe")
-  #     fill_in("Email", :with => "joe@joe.com")
-  #     fill_in("Password", :with => "pafdkjfadjl;fjdklsa;jfkldsjfkl;adsjfl;ss")
-  #     click_button("Sign Up")
-  #     expect(page).to have_content("must be less than 15 characters.")
-  #   end
+    it "fails with invalid email", js: true do
+      visit root_path
+      click_link("Signup")
+      fill_in("user[first_name]", :with => "Taco")
+      fill_in("user[last_name]", :with => "Meat")
+      fill_in("user[ravelry_name]", :with => "meaty")
+      fill_in("user[email]", :with => "me")
+      fill_in("user[password]", :with => "password1234")
+      click_button("Signup")
+      page.should have_content("Must be a valid email address!")
+    end
   end
 end
